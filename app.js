d8b5b39df1194a6f416f79357d7feafbe8bcd316
Vikas Rajbhar <vikasrajbhar00@gmail.com>
2019-11-13 22:39:47 +0530

    Added Edit and Update Route/Form, Method-Override

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var bodyParser = require("body-parser");
-    mongoose   = require("mongoose");
-    express    = require("express");
-    app        = express();
+var methodOverride = require("method-override"),
+    bodyParser     = require("body-parser");
+    mongoose       = require("mongoose");
+    express        = require("express");
+    app            = express();
 
 
 // App Config
-mongoose.connect("mongodb://localhost:27017/blogApp",{ useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/blogApp",{ useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+mongoose.set('useFindAndModify', false);
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
 
 
 // Mongoose/Model Config
@@ -73,6 +76,29 @@ app.get("/blogs/:id", function(req, res) {
     });
 });
 
+
+// Edit Route
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+
+// Update Route
+app.put("/blogs/:id", function(req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
 
 
 app.listen(3000, function(){
